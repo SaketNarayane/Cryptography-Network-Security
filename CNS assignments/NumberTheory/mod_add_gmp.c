21c46b294f8a1b95a52564d9fed05c218418c86a
Saket Narayane <snarayane@student.nitw.ac.in>
2017-08-27 19:49:39 +0530

    CNS files

---
+++ CNS assignments/NumberTheory/mod_add_gmp.c
@@ -0,0 +1,26 @@
+/*
+ * Modular arithmetic addition
+ *
+ */
+
+/**
+ * (a+b)%n = (a%n + b%n)%n
+ */
+ 
+#include <gmp.h>
+#include <stdio.h>
+
+int main() {
+    mpz_t a, b, n, x, y;
+    mpz_inits(a, b, n, x, y, (mpz_ptr)NULL);
+    printf("Enter Both Numbers\n");
+    gmp_scanf("%Zd %Zd", a, b);
+    printf("Enter number to get modulo with: ");
+    gmp_scanf("%Zd", n);
+    mpz_mod(x, a, n);
+    mpz_mod(y, b, n);
+    mpz_add(x, x, y);
+    mpz_mod(x, x, n);
+    gmp_printf("(%Zd + %Zd) mod %Zd = %Zd\n", a, b, n, x);
+    return 0;
+}
